a12d632870655732795c47b7726939d4ce18a937
h_sean <guccilynx01@live.com>
2025-02-26 00:06:12 -0500

    ClassList used to add effects when button is clicked.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var button = document.querySelector(".show-cat");
+//console.log(button);
+
+var cat = document.querySelector(".cat");
+//console.log(cat.innerHTML);
+
+button.addEventListener("click", function() {
+if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+} else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+}
+});
